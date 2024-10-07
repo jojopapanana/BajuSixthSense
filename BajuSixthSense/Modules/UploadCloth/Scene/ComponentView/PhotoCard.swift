ba4c442f3c042d67425ea46c48a4be77f2664e76
Vans <stevanstc2018@gmail.com>
2024-10-07 15:25:29 +0700

    FInish upload photo card revision.

--- BajuSixthSense/Modules/UploadCloth/Scene/ComponentView/PhotoCard.swift
+++ BajuSixthSense/Modules/UploadCloth/Scene/ComponentView/PhotoCard.swift
@@ -10,7 +10,7 @@ import PhotosUI
 
 struct PhotoCard: View {
     @State var chosenPhoto: PhotosPickerItem?
-    @State var displayCloth: UIImage?
+    @State var chosenCloth: UIImage?
     @State var galleryUpload: Bool = false
     @State var cameraUpload: Bool = false
     
@@ -18,43 +18,49 @@ struct PhotoCard: View {
     
     var body: some View {
         ZStack {
-            if displayCloth != nil {
-                ZStack {
-                    Image(uiImage: displayCloth!)
-                        .resizable()
-                        .scaledToFill()
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                    
-                    Image(systemName: "trash.circle.fill")
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.white, .black)
+            if chosenCloth != nil {
+                Image(uiImage: chosenCloth!)
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "trash.circle.fill")
+                            .font(.system(size: 32))
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(.white, .black)
+                            .onTapGesture {
+                                chosenCloth = nil
+                            }
+                    }
+                    Spacer()
                 }
-                .onTapGesture {
-                    displayCloth = nil
-                }
+                .frame(width: width)
             } else {
-                Menu {
-                    Button {
-                        cameraUpload.toggle()
-                    } label: {
-                        Label("Take Photo", systemImage: "camera")
-                    }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 2.13)
+                        .foregroundColor(.clear)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 2.13)
+                                .inset(by: 0.43)
+                                .stroke(.black, style: StrokeStyle(lineWidth: 0.43, dash: [4.25, 2.13]))
+                        )
                     
-                    Button {
-                        galleryUpload.toggle()
+                    Menu {
+                        Button {
+                            cameraUpload.toggle()
+                        } label: {
+                            Label("Take Photo", systemImage: "camera")
+                        }
+                        
+                        Button {
+                            galleryUpload.toggle()
+                        } label: {
+                            Label("Choose Photo", systemImage: "photo.on.rectangle")
+                        }
                     } label: {
-                        Label("Choose Photo", systemImage: "photo.on.rectangle")
-                    }
-                } label: {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 2.13)
-                            .foregroundColor(.clear)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 2.13)
-                                    .inset(by: 0.43)
-                                    .stroke(.black, style: StrokeStyle(lineWidth: 0.43, dash: [4.25, 2.13]))
-                            )
                         Image(systemName: "plus.circle.fill")
                             .font(.system(size: 32))
                             .symbolRenderingMode(.palette)
@@ -68,7 +74,7 @@ struct PhotoCard: View {
         .overlay(
             RoundedRectangle(cornerRadius: 5)
                 .inset(by: 1)
-                .stroke(.black, lineWidth: displayCloth == nil ? 0 : 1)
+                .stroke(.black, lineWidth: chosenCloth == nil ? 0 : 1)
         )
         .photosPicker(
             isPresented: $galleryUpload,
@@ -76,13 +82,13 @@ struct PhotoCard: View {
             matching: .any(of: [.images, .screenshots])
         )
         .fullScreenCover(isPresented: $cameraUpload) {
-            ImagePicker(chosenImage: $displayCloth)
+            ImagePicker(chosenImage: $chosenCloth)
                 .ignoresSafeArea()
         }
         .onChange(of: chosenPhoto) { oldValue, newValue in
             Task {
                 if let photo = try? await chosenPhoto?.loadTransferable(type: Data.self) {
-                    displayCloth = UIImage(data: photo)
+                    chosenCloth = UIImage(data: photo)
                 }
             }
         }
