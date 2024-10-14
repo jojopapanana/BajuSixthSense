a640227b689b7522cdbe12cf644c32696732aaab
Vans <stevanstc2018@gmail.com>
2024-10-14 17:30:04 +0700

    [LOGIC]: Adding new url sharing manager. [FEAT]: Adding sharing functionality/feature to the project.

--- BajuSixthSense/Infrastructure/URLSharingManager.swift
+++ BajuSixthSense/Infrastructure/URLSharingManager.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import SwiftUI
+
+class URLSharingManager {
+    static let shared = URLSharingManager()
+    
+    func chatInWA(phoneNumber: String, textMessage: String) {
+        let urlHeader = "https://wa.me/"
+        let urlString = urlHeader + phoneNumber + "?text=" + textMessage
+        let urlStringEncoded = urlString
+            .addingPercentEncoding(
+                withAllowedCharacters: .urlQueryAllowed
+            ) ?? urlHeader + phoneNumber
+        guard let url = URL(string: urlStringEncoded) else {
+            return
+        }
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    func generateShareLink(clothID: String) -> URL {
+        let appLinkHeader = "KeLothing://"
+        let host = "clothDetail"
+        let appLink = URL(string: appLinkHeader + host + "?id=" + clothID)
+        return appLink ?? URL(string: appLinkHeader)!
+    }
+    
+    func retreiveClothID(from link: URL) -> String {
+        guard let retrieveID = link.queryParam?["id"] else {
+            return ""
+        }
+        
+        return retrieveID
+    }
+    
+}
+
+extension URL {
+    var queryParam: [String: String]? {
+        guard
+            let component = URLComponents(url: self, resolvingAgainstBaseURL: true),
+            let items = component.queryItems
+        else {
+            return nil
+        }
+        
+        return items.reduce(into: [String: String]()) { (result, item) in
+            let name = item.name
+            let value = item.value
+            result[name] = value
+        }
+    }
+}
